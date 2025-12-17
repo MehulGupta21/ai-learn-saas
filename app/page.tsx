53d317e60523c2bc61250dca22abfd586776c540
Mehul Gupta <70238140+MehulGupta21@users.noreply.github.com>
2025-12-17 13:09:23 +0530

    Working on Homepage Design

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,45 @@
 import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import Cta from "@/components/CTA";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
-        <Button>
-            Let's get Started
-        </Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+
+        <section className="home-section">
+            <CompanionCard
+                id="123"
+                name="Neura the Brainy Explorer"
+                topic="Neural Network of the Brain"
+                subject="Science"
+                duration={45}
+                color="#ffda6e"
+            />
+            <CompanionCard
+                id="456"
+                name="Countsy the Number Wizard"
+                topic="Derivatives & Integrals"
+                subject="Maths"
+                duration={30}
+                color="#e5d0ff"
+            />
+            <CompanionCard
+                id="789"
+                name="Verba the Vocabulary Builder"
+                topic="English Literature"
+                subject="Language"
+                duration={30}
+                color="#bde7ff"
+            />
+        </section>
+
+        <section className="home-section">
+            <CompanionsList/>
+            <Cta/>
+        </section>
+    </main>
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
             <Link href="/">
                 <div className="flex items-center gap-2.5 cursor-pointer">
                     <Image
-                        src="/Users/mehulgupta/Desktop/ai_learn/public/images/logo.svg"
+                        src="images/logo.svg"
                         alt="Logo"
                         width={46}
                         height={44}
